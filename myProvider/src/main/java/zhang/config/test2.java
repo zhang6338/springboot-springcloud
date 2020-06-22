52420a8162f89e8fb730ff6725c3f68b7df0bd61
zhang6338 <1345512331@qq.com>
2020-06-22 09:16:10 +0800

    测试2

---
+++ myProvider/src/main/java/zhang/config/test2.java
@@ -0,0 +1,11 @@
+package zhang.config;
+
+public class test2 {
+
+    public void test2(){
+        System.out.println("tset222");
+        System.out.println("tset222");
+        System.out.println("tset222");
+        System.out.println("tset222");
+    }
+}
